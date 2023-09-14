11eb1d40d54cd910df8f5d1d4b21f5f938991c0e
Emily-Campbell23 <emcampbel@hartford.edu>
2023-09-14 15:26:30 -0400

    added variables, working on Fahrenheit to Celsius

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,23 @@
-// Import libraries here
-
+import java.util.Scanner;
+/*Goal: Print out initials using letters, E and C. User input, asking for 
+degrees Fahrenheit and convert to Celsius, User input, asking for a 5 character string,
+reverse it and remove first and last characters, generate a random number between 32 and 16,384 
+inclusive, print everything out
+ */ 
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+    int degrees = 0; // variable for degrees Fahrenheit
+    int randomNumber = 0; // variables for a random Number
+    String userString = ""; // variable for users string
+
+
+
+    // Fahrenheit to Celsius
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter a number in degrees Fahrenheit: ");
+
+    
+  
+
   }
 }
